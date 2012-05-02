11a8cd5d5e2cf9119850c7bbb42494f2433ccdf1
Todd Gamblin <tgamblin@llnl.gov>
2012-05-02 11:23:48 -0700

    Update tiltview to use view_in_app

--- tiltview.py
+++ tiltview.py
@@ -3,14 +3,10 @@
 import sys
 import rubik.rubikview as rv
 from rubik import *
-from PySide.QtCore import *
-from PySide.QtGui import *
 
-def main():
-    app = QApplication(sys.argv)    # Create a Qt application
-
-    p = box([8, 8, 8])
-    p.tile([8,2,4])
+if __name__ == "__main__":
+    p = box([8,8,8])
+    p.tile([1])
     p.traverse_cells(rv.assign_flat_index_gradient_color)
 
     q = box([8,8,8])
@@ -21,21 +17,4 @@ def main():
 #        child.tilt(0,1,1)
 #        child.tilt(0,2,1)
 
-    mainwindow = QMainWindow()
-
-#    renderer = rv.make_nested_faces
-#    renderer = rv.make_leaf_faces
-    renderer = rv.make_colored_faces
-    glview = rv.RubikView(q, renderer, mainwindow)
-
-    mainwindow.setCentralWidget(glview)
-    mainwindow.resize(800, 600)
-    mainwindow.move(30, 30)
-
-    mainwindow.show()
-    app.exec_()    # Enter Qt application main loop
-
-
-if __name__ == "__main__":
-    main()
-    sys.exit()
+    rv.view_in_app(p, rv.ColoredFaceRenderer())
