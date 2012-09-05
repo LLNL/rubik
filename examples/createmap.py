0358b892f75e9880ff1720bf7b8de3ff763cdccf
Abhinav Bhatele <bhatele@llnl.gov>
2012-09-05 16:17:58 -0700

    remove application specific mappings and reorganize into tests and examples

---
+++ examples/createmap.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2.6
+
+from rubik import *
+
+if __name__ == "__main__":
+    # application topology
+    app = box([8, 8])
+    app.tile([4, 4])
+    print app.box
+    print ""
+
+    # processor topology
+    torus = box([4, 4, 4])
+    torus.tile([1, 4, 4])
+
+    torus.map(app)
+    print torus.box
+    print ""
+
+    f = open('mapfile', 'w')
+    torus.write_map_file(f)
+    f.close()
---
+++ examples/zigzagview.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from rubik import *
+import rubik.rubikview as rv
+
+if __name__ == "__main__":
+    p = box([8, 8, 8])
+    p.div([2, 2, 2])
+    p.traverse_cells(rv.assign_flat_index_gradient_color)
+
+    p.zigzag(1, 0, 1, 1)
+    p.zigzag(2, 1, 1, 1)
+    p.zigzag(0, 2, 1, 1)
+
+    rv.view_in_app(p, rv.ColoredFaceRenderer())
