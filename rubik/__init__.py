a7986caabf7bee2bdd8d1da9fdc9fd4a0aefd1ba
Abhinav Bhatele <bhatele@llnl.gov>
2012-09-16 05:05:42 +0000

    append the location of site-packages on Intrepid

--- rubik/__init__.py
+++ rubik/__init__.py
@@ -8,7 +8,8 @@ Contributors:  Abhinav Bhatele bhatele@llnl.gov
                Martin Schulz schulzm@llnl.gov
 """
 import sys
-sys.path.append('/soft/apps/python/python-2.6.6-fen-gcc/usr/lib64/python2.6/site-packages')
+sys.path.append('/soft/apps/python/python-2.6.4-fen-gcc/python/lib/python2.6/site-packages') # Intrepid at Argonne
+sys.path.append('/soft/apps/python/python-2.6.6-fen-gcc/usr/lib64/python2.6/site-packages') # Vesta at Argonne
 
 import zorder
 from partition import *
