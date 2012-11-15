0a0a73e7c00a480cc9fe441c28a2b8415395fec4
Todd Gamblin <tgamblin@llnl.gov>
2012-11-15 04:05:08 -0800

    Added license file to source.

--- tests/zorder-test.py
+++ tests/zorder-test.py
@@ -1,27 +1,28 @@
+#!/usr/bin/env python
 ################################################################################
 # Copyright (c) 2012, Lawrence Livermore National Security, LLC.
 # Produced at the Lawrence Livermore National Laboratory
 # Written by Todd Gamblin et al. <tgamblin@llnl.gov>
 # LLNL-CODE-599252
 # All rights reserved.
-# 
+#
 # This file is part of Rubik. For details, see http://scalability.llnl.gov.
 # Please read the LICENSE file for further information.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     • Redistributions of source code must retain the above copyright notice,
 #       this list of conditions and the disclaimer below.
-# 
+#
 #     • Redistributions in binary form must reproduce the above copyright notice,
 #       this list of conditions and the disclaimer (as noted below) in the
 #       documentation and/or other materials provided with the distribution.
-# 
+#
 #     • Neither the name of the LLNS/LLNL nor the names of its contributors may be
 #       used to endorse or promote products derived from this software without
 #       specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -34,24 +35,30 @@
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ################################################################################
-"""
-This file defines the Process class, used to represent MPI ranks in Rubik.
-"""
 
-class Process(object):
-    """ The process class represents a single task in a parallel application
-    with a unique identifier. Identifiers can be anything.
-    """
-    def __init__(self, id):
-	""" Constructs a process with a particular id, optionally as part of a
-	list.
+from rubik.zorder import *
 
-        Parameters:
-	    id		arbitrary process identifier.
-        """
-        self.id      = id
-        self.coord   = None
+def test_ordering(shape):
+    """ Construct a Z encoder for the given shape, then """
+    zencoder = ZEncoder.for_shape(shape)
+    print zencoder
+    for point in np.ndindex(*shape):
+        code = zencoder.encode(point)
+        decoded = zencoder.decode(code)
+        if point != decoded:
+            raise Exception("Error for code got %s but expected %s for code %s" % (decoded, point, b(code)))
 
-    def __str__(self):
-        """ String representation for printing is just the identifier. """
-        return "%3d" % self.id
+
+if __name__ == "__main__":
+    test_ordering([1024])
+    print "1D passed."
+    test_ordering([64,32])
+    print "2D passed."
+    test_ordering([8,16,32])
+    print "3D passed."
+    test_ordering([8,4,32,128])
+    print "4D passed."
+    test_ordering([8,4,16,16,32])
+    print "5D passed."
+    test_ordering([8,2,8,32,4,4])
+    print "6D passed."
