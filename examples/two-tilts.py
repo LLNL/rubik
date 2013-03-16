c22d524b3a2853c6199f25cb1b72d5816d532ad8
Abhinav Bhatele <bhatele@llnl.gov>
2013-03-16 11:46:40 -0700

    two tilts to use all 3 directions for comm in Z

---
+++ examples/two-tilts.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from rubik import *
+import rubik.view as rv
+
+Z, Y, X = 0, 1, 2  # Assign names to dimensions
+t0 = box([4, 4, 4]) # Create a box
+t0.tile([4, 4, 1])
+for child in t0:
+    child.tile([4, 1, 1])
+rv.color(t0)
+
+t1 = t0.copy()
+t1.tilt(X, Z, 1)  # Tilt Z (XY) planes along X
+
+t2 = t1.copy()
+t2.tilt(Y, Z, 1)  # Tilt X (YZ) planes along Y
+
+rv.viewbox(t0, t1, t2)
